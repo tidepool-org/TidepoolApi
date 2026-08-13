3f56be5fa23ef852816c08c76508a95514e15a77
Eric Wollesen <eric@tidepool.org>
2026-08-13 14:21:43 +0200

    reformat scripts/generate_clinic.sh
    Using shfmt --indent=4 --binary-next-line
    
    A future commit will make some changes, so I'm just getting the
    reformatting out of the way now.

--- scripts/generate_clinic.sh
+++ scripts/generate_clinic.sh
@@ -2,46 +2,47 @@
 
 set -eou pipefail
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-if [[ $OSTYPE =~ darwin ]] && command -v grealpath &> /dev/null; then
-  TOOLS_BIN=$(grealpath --canonicalize-existing "$SCRIPT_DIR/../tools/bin")
-  NPM_BIN=$(grealpath --canonicalize-existing "$SCRIPT_DIR/../node_modules/.bin")
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+if [[ $OSTYPE =~ darwin ]] && command -v grealpath &>/dev/null; then
+    TOOLS_BIN=$(grealpath --canonicalize-existing "$SCRIPT_DIR/../tools/bin")
+    NPM_BIN=$(grealpath --canonicalize-existing "$SCRIPT_DIR/../node_modules/.bin")
 else
-  TOOLS_BIN=$(realpath --canonicalize-existing "$SCRIPT_DIR/../tools/bin")
-  NPM_BIN=$(realpath --canonicalize-existing "$SCRIPT_DIR/../node_modules/.bin")
+    TOOLS_BIN=$(realpath --canonicalize-existing "$SCRIPT_DIR/../tools/bin")
+    NPM_BIN=$(realpath --canonicalize-existing "$SCRIPT_DIR/../node_modules/.bin")
 fi
 PATH=$TOOLS_BIN:$NPM_BIN:$PATH
 
 trace() {
-	if [ "${QUIET:-}" = "true" ]; then
-		output=$(echo "${PS4}$*" && "$@" 2>&1) || ( echo "${output}" && exit 1 )
-	else
-		echo "${PS4}$*"
-		"$@"
-	fi
+    if [ "${QUIET:-}" = "true" ]; then
+        output=$(echo "${PS4}$*" && "$@" 2>&1) || (echo "${output}" && exit 1)
+    else
+        echo "${PS4}$*"
+        "$@"
+    fi
 }
 
 case $1 in
-    -h | --help)
-	echo "usage: $(basename "$0") [-h | --help] | [-c | --self-check] | {source-spec-filename} {bundled-spec-filename}"
-	;;
+-h | --help)
+    echo "usage: $(basename "$0") [-h | --help] | [-c | --self-check] | {source-spec-filename} {bundled-spec-filename}"
+    ;;
 
-    -c | --self-check)
-	trace redocly --version
-	trace oapi-codegen --version
-	;;
+-c | --self-check)
+    trace redocly --version
+    trace oapi-codegen --version
+    ;;
 
-    *)
-	source=${1?:source-spec-filename is required}
-	bundled=${2?:bundled-spec-filename is required}
-	server="$(dirname "$bundled")/server"
-	client="$(dirname "$bundled")/client"
-	common=( --old-config-style --exclude-tags=Confirmations --package=api )
-	REDOCLY_SUPPRESS_UPDATE_NOTICE="true" trace redocly bundle "$source" -o "$bundled"
-	mkdir -p "$server" "$client"
-	trace oapi-codegen "${common[@]}" --generate=server -o "$server/gen_server.go" "$bundled"
-	trace oapi-codegen "${common[@]}" --generate=spec -o "$server/gen_spec.go" "$bundled"
-	trace oapi-codegen "${common[@]}" --generate=types -o "$server/gen_types.go" "$bundled"
-	trace oapi-codegen "${common[@]}" --generate=types -o "$client/types.go" "$bundled"
-	trace oapi-codegen "${common[@]}" --generate=client -o "$client/client.go" "$bundled"
+*)
+    source=${1?:source-spec-filename is required}
+    bundled=${2?:bundled-spec-filename is required}
+    server="$(dirname "$bundled")/server"
+    client="$(dirname "$bundled")/client"
+    common=(--old-config-style --exclude-tags=Confirmations --package=api)
+    REDOCLY_SUPPRESS_UPDATE_NOTICE="true" trace redocly bundle "$source" -o "$bundled"
+    mkdir -p "$server" "$client"
+    trace oapi-codegen "${common[@]}" --generate=server -o "$server/gen_server.go" "$bundled"
+    trace oapi-codegen "${common[@]}" --generate=spec -o "$server/gen_spec.go" "$bundled"
+    trace oapi-codegen "${common[@]}" --generate=types -o "$server/gen_types.go" "$bundled"
+    trace oapi-codegen "${common[@]}" --generate=types -o "$client/types.go" "$bundled"
+    trace oapi-codegen "${common[@]}" --generate=client -o "$client/client.go" "$bundled"
+    ;;
 esac
